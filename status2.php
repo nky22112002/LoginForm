cfa6bbaee29320b2f1367b837c8636a37ed27730
minusnineteen <117788629+minusnineteen@users.noreply.github.com>
2023-03-10 15:44:26 +0700

    Add files via upload

---
+++ status2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Xóa dữ liệu từ Database</title>
+  </head>
+  <body>
+    <h1>Xóa dữ liệu từ Database</h1>
+
+    <form method="post">
+      <label for="username">Tên người dùng:</label>
+      <input type="text" name="username" id="username">
+      <button type="submit" name="delete">Xóa</button>
+    </form>
+
+    <?php
+        // Xử lý biểu mẫu khi nút Xóa được nhấp
+        if(isset($_POST['delete'])) {
+        $username = $_POST['username'];
+
+        // Tạo kết nối đến cơ sở dữ liệu
+        $conn = new mysqli("localhost", "root", "", "brinets");
+
+        // Xóa dữ liệu từ bảng users dựa trên tên người dùng
+        $sql = "DELETE FROM user WHERE username='$username' AND status = 1";
+        $result = $conn->query($sql);
+
+        if($result) {
+            echo 'Xóa dữ liệu thành công.';
+        } else {
+            echo 'Lỗi: Không thể xóa dữ liệu.';
+        }
+
+        // Đóng kết nối
+        $conn->close();
+        }   
+    ?>
+  </body>
+</html>
