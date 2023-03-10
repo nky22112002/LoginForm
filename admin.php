1eb4317daaa634a632cc039ad7632f4629f94138
ky <nky22112002@gmail.com>
2023-03-10 10:03:14 +0700

    get value of status

--- admin.php
+++ admin.php
@@ -1,24 +1,40 @@
 <?php
-    // Kết nối tới cơ sở dữ liệu
-    $mysqli = new mysqli("localhost", "root", "", "brinets");
+    // // Kết nối tới cơ sở dữ liệu
+    // $mysqli = new mysqli("localhost", "root", "", "brinets");
 
-    // Thực hiện truy vấn để lấy dữ liệu
-    $result = $mysqli->query("SELECT * FROM user");
-    // Tạo danh sách HTML từ dữ liệu
-    echo "<ul>";
-    while ($row = $result->fetch_assoc()) {
-        echo "<li>" . $row["username"] . "</li>";
-        echo "<button>". "<a href='do_admin.php'>Delete</a>" . "</button>"; 
-        // echo "<button>" . $sql = "DELETE FROM user WHERE username = 'ky12345'" . $mysqli->query($sql); . "</button>"; 
-    }
-    echo "</ul>";
-    // Xóa
-    // $sql = "DELETE FROM user WHERE username = 'ky12345'";
-	// if ($mysqli->query($sql) === TRUE) {
-    //     echo "Xóa hàng dữ liệu thành công";
-    // } else {
-    //     echo "Lỗi: " . mysqli_error($mysqli);
+    // // Thực hiện truy vấn để lấy dữ liệu
+    // $result = $mysqli->query("SELECT * FROM user");
+    // // Tạo danh sách HTML từ dữ liệu
+    // $sql = "DELETE FROM user WHERE username = 'abcdef'";
+    // echo "<ul>";
+    // while ($row = $result->fetch_assoc()) {
+    //     echo "<li>" . $row["username"] . "</li>";
+    //     echo "<button>". "<a href='do_admin.php'>Delete</a>" . "</button>"; 
+    //     echo "<button>" . $sql ."</button>"; 
+
     // }
+    // echo "</ul>";
+    // // Xóa
+    // // $sql = "DELETE FROM user WHERE username = 'ky12345'";
+	// // if ($mysqli->query($sql) === TRUE) {
+    // //     echo "Xóa hàng dữ liệu thành công";
+    // // } else {
+    // //     echo "Lỗi: " . mysqli_error($mysqli);
+    // // }
     
-
+require 'header.php';
 ?>
+<main>
+    <?php
+        echo "<ul>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<li>" . $row["username"] . "</li>";
+            echo "<button>". "<a href='do_admin.php'>Delete</a>" . "</button>"; 
+            echo "<button>" . $sql ."</button>"; 
+
+        }
+        echo "</ul>";
+    ?>
+        <li></li>
+</main>
+
--- do_admin.php
+++ do_admin.php
@@ -1,11 +1,14 @@
 <?php
+
 // Kết nối tới cơ sở dữ liệu
 $mysqli = new mysqli("localhost", "root", "", "brinets");
 
 $sql = "DELETE FROM user WHERE username = 'admin'";
 	if ($mysqli->query($sql) === TRUE) {
-        echo "Xóa hàng dữ liệu thành công";
+        echo "Xóa hàng dữ liệu thành công ";
+        
     } else {
         echo "Lỗi: " . mysqli_error($mysqli);
     }
+   
 ?>
