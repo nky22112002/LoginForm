b35f0a644748f26fa64ffbb3ed5b826df9a5de31
ky <nky22112002@gmail.com>
2023-03-10 10:11:39 +0700

    get status

--- admin.php
+++ admin.php
@@ -1,14 +1,24 @@
 <?php
+require 'header.php';
     // Kết nối tới cơ sở dữ liệu
     $mysqli = new mysqli("localhost", "root", "", "brinets");
 
     // Thực hiện truy vấn để lấy dữ liệu
     $result = $mysqli->query("SELECT * FROM user");
     // Tạo danh sách HTML từ dữ liệu
+    $sql1 = "SELECT status FROM user WHERE username = '$username'" ;
+    $result1 = $mysqli->query($sql1);
+    $row1 = $result1->fetch_assoc();
+    $status = $row1['status'];
+    echo strval($status);
     echo "<ul>";
     while ($row = $result->fetch_assoc()) {
         echo "<li>" . $row["username"] . "</li>";
-        echo "<button>". "<a href='do_admin.php'>Delete</a>" . "</button>"; 
+        echo "<button>" . "<a href='do_admin.php'>Delete</a>" . "</button>";
+        
+        
+        
+        // echo "<button>". "<a href='do_admin.php'>Delete</a>" . "</button>"; 
         // echo "<button>" . $sql = "DELETE FROM user WHERE username = 'ky12345'" . $mysqli->query($sql); . "</button>"; 
     }
     echo "</ul>";
--- do_admin.php
+++ do_admin.php
@@ -1,10 +1,25 @@
 <?php
+require 'header.php';
+
 // Kết nối tới cơ sở dữ liệu
 $mysqli = new mysqli("localhost", "root", "", "brinets");
 
 $sql = "DELETE FROM user WHERE username = 'admin'";
+// $sql1 = "SELECT status FROM user WHERE username='$username'" ;
+// $result = $mysqli->query($sql1);
+// if ($result->num_rows > 0) {
+//     // User exists, get their status
+//     $row = $result->fetch_assoc();
+//     $status = $row['status'];
+    
+// } else {
+//     // User does not exist or incorrect credentials
+//     $status = "invalid";
+// }
+// // Now you can use $status without triggering the warning
+// echo "User status is: " . $status;
 	if ($mysqli->query($sql) === TRUE) {
-        echo "Xóa hàng dữ liệu thành công";
+        echo "Xóa hàng dữ liệu thành công ";
     } else {
         echo "Lỗi: " . mysqli_error($mysqli);
     }
