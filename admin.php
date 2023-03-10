9a7235181d3633711fc067674dddee0b024e003c
ky <nky22112002@gmail.com>
2023-03-10 10:40:21 +0700

    Authorize successfully

--- admin.php
+++ admin.php
@@ -11,13 +11,33 @@ require 'header.php';
 
     $row1 = $result1->fetch_assoc();
     $status = $row1['status'];
-    echo $status;
     echo "<ul>";
+    if($status == 1){
     while ($row = $result->fetch_assoc()) {
-        echo "<li>" . $row["username"] . "</li>";
-        echo "<button>". "<a href='do_admin.php'>Delete</a>" . "</button>"; 
-        // echo "<button>" . $sql = "DELETE FROM user WHERE username = 'ky12345'" . $mysqli->query($sql); . "</button>"; 
+            echo "<li>" . $row["username"] . "</li>";
+            // echo "<button>". "<a href='do_admin.php'>Delete</a>" . "</button>"; 
+            // echo "<button>" . $sql = "DELETE FROM user WHERE username = 'ky12345'" . $mysqli->query($sql); . "</button>"; 
+        }
     }
+    if($status == 2){
+        while ($row = $result->fetch_assoc()) {
+            echo "<li>" . $row["username"] . "</li>";
+            if($row["status"] == 1){
+                echo "<button>". "<a href='do_admin.php'>Delete</a>" . "</button>"; 
+            }
+            // echo "<button>" . $sql = "DELETE FROM user WHERE username = 'ky12345'" . $mysqli->query($sql); . "</button>"; 
+        }
+    }
+    if($status == 3){
+        while ($row = $result->fetch_assoc()) {
+            echo "<li>" . $row["username"] . "</li>";
+            echo "<button>". "<a href='do_admin.php'>Delete</a>" . "</button>"; 
+            // echo "<button>" . $sql = "DELETE FROM user WHERE username = 'ky12345'" . $mysqli->query($sql); . "</button>"; 
+        }
+    }
+
+
+    
     echo "</ul>";
     // Xóa
     // $sql = "DELETE FROM user WHERE username = 'ky12345'";
